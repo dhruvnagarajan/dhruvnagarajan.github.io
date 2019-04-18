a11ecc4ee147376c14dce45f8869b12331b11e7a
Dhruv Nagarajan <emarld2011@gmail.com>
2019-04-18 22:11:19 +0530

    + Profile border, + KnGraph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Profile from './components/profile/profile';
+import Profile from './components/profile/profile.component';
 import KnGraph from './components/skills/kngraph/kngraph.component';
 
 export default class App extends React.Component {
